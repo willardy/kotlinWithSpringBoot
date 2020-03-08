1c90814c4a5c3bc815ceddbf1d34a6948372dbdd
Willardy Tyrone Luceno de Oliveira <willardy@gmail.com>
2020-03-08 11:19:02 -0300

    bug fixes and create gitignore

--- src/test/kotlin/br/com/willardy/kotlinspringboot/KotlinspringbootApplicationTests.kt
+++ src/test/kotlin/br/com/willardy/kotlinspringboot/KotlinspringbootApplicationTests.kt
@@ -3,7 +3,6 @@ package br.com.willardy.kotlinspringboot
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class KotlinspringbootApplicationTests {
 
 	@Test
